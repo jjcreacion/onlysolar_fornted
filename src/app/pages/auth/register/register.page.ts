f672b918da158be536ef6f21f4da33602b4b593b
jjcreacion <jjcreacion@gmail.com>
2022-11-07 18:13:25 -0500

    Correciones 7/11

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -84,6 +84,9 @@ export class RegisterPage implements OnInit {
   isSubmitted = false;
 
   idfile: any;
+  ssfile: any;
+  checkfile: any;
+  taxfile: any; 
 
   constructor(
     public formBuilder: FormBuilder,
@@ -97,43 +100,48 @@ export class RegisterPage implements OnInit {
       if(params.code) this.referred = params.code
     })
   }
+  patternNum = /[0-9]/;
+  patternLett = /[A-Za-z ÑñáéíóúÁÉÍÓÚ]/;
+  patternNumLett = /[0-9A-Za-z ÑñáéíóúÁÉÍÓÚ.]/; 
+  patternEmail = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$';
+  patternPassword = '(?=\\D*\\d)(?=[^a-z]*[a-z])(?=[^A-Z]*[A-Z]).{8,14}';
 
   ngOnInit() {
     this.ionicForm = this.formBuilder.group({
       referred: [null],
-      email: [null, [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
-      name: [null, [Validators.required]],
-      password: [null, [Validators.required]],
-      password2: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.pattern(this.patternEmail)]],
+      name: [null, [Validators.required,Validators.minLength(3),Validators.pattern(this.patternLett)]],
+      middlename: [null, [Validators.pattern(this.patternLett)]],
+      password: [null, [Validators.required,Validators.pattern(this.patternPassword)]],
+      password2: [null, [Validators.required,Validators.pattern(this.patternPassword)]],
       document: [null, [Validators.required]],
-      lastname: [null, [Validators.required]],
+      lastname: [null, [Validators.required,Validators.minLength(3),Validators.pattern(this.patternLett)]],
       numberid: [null],
       numberid2: [null],
       numberss: [null],
       numbertax: [null],
-      phone: [null, [Validators.required]],
-      residenceAddress: [null, [Validators.required]],
-      residenceState: [null, [Validators.required]],
-      residenceCity: [null, [Validators.required]],
-      contactFullName: [null, [Validators.required]],
-      contactRelationship: [null, [Validators.required]],
-      contactAddress: [null, [Validators.required]],
-      contactPhone: [null, [Validators.required]],
-      bankaccountNamebank: [null, [Validators.required]],
-      bankaccountRouting: [null, [Validators.required]],
-      bankaccountAccount: [null, [Validators.required]],
+      phone: [null, [Validators.required, Validators.minLength(10), Validators.pattern(this.patternNum)]],
+      residenceAddress: [null, [Validators.required,Validators.minLength(5), Validators.pattern(this.patternNumLett)]],
+      residenceState: [null, [Validators.required,Validators.minLength(4), Validators.pattern(this.patternLett)]],
+      residenceCity: [null, [Validators.required,Validators.minLength(4), Validators.pattern(this.patternLett)]],
+      residenceZipCode: [null, [Validators.required,Validators.minLength(4), Validators.pattern(this.patternNum)]],
+      contactFullName: [null, [Validators.required,Validators.minLength(5),Validators.pattern(this.patternLett)]],
+      contactRelationship: [null, [Validators.required,Validators.minLength(3),Validators.pattern(this.patternLett)]],
+      contactAddress: [null, [Validators.minLength(5), Validators.pattern(this.patternNumLett)]],
+      contactPhone: [null, [Validators.required, Validators.minLength(10), Validators.pattern(this.patternNum)]],
+      bankaccountNamebank: [null, [Validators.required,Validators.minLength(5),Validators.pattern(this.patternLett)]],
+      bankaccountRouting: [null, [Validators.required,Validators.minLength(9),Validators.pattern(this.patternNum)]],
+      bankaccountAccount: [null, [Validators.required,Validators.minLength(9),Validators.pattern(this.patternNum)]],
       bankaccountPercentage: [null, [Validators.required]],
-      previousemploymentCompany: [null, [Validators.required]],
-      previousemploymentResponsabilities: [null, [Validators.required]],
-      previousemploymentReasonleaving: [null, [Validators.required]],
-      previousemploymentAddress: [null, [Validators.required]],
-      previousemploymentSupervisor: [null, [Validators.required]],
-      previousemploymentPhone: [null, [Validators.required]],
-      previousemploymentStarsalary: [null, [Validators.required]],
-      previousemploymentStartDate: [null, [Validators.required]],
-      previousemploymentEndDate: [null, [Validators.required]],
-      previousemploymentEndsalary: [null, [Validators.required]],
-      previousemploymentContactsupervisor: [null, [Validators.required]],
+      previousemploymentCompany: [null, [Validators.required, Validators.minLength(3), Validators.pattern(this.patternNumLett)]],
+      previousemploymentResponsabilities: [null, [Validators.required,Validators.minLength(5), Validators.pattern(this.patternNumLett)]],
+      previousemploymentReasonleaving: [null, [Validators.required,Validators.minLength(5), Validators.pattern(this.patternNumLett)]],
+      previousemploymentAddress: [null, [Validators.pattern(this.patternNumLett)]],
+      previousemploymentSupervisor: [null, [Validators.pattern(this.patternLett)]],
+      previousemploymentPhone: [null, [Validators.pattern(this.patternNum)]],
+      previousemploymentStarsalary: [null, [Validators.pattern(this.patternNum)]],
+      previousemploymentEndsalary: [null, [Validators.pattern(this.patternNum)]],
+      previousemploymentContactsupervisor: [null,[Validators.pattern(this.patternNum)]],
     })
   }
 
@@ -177,10 +185,18 @@ export class RegisterPage implements OnInit {
     }
   }
 
-  loadImageFromDevice(e) {
-    this.idfile = e.target.files[0]
-    console.log(this.idfile)
-  }
+  loadImageFromDevice(e,tipo) {
+    switch(tipo){
+     case 1:this.idfile = e.target.files[0]
+     break
+     case 2:this.ssfile = e.target.files[0]
+     break
+     case 3:this.taxfile = e.target.files[0]
+     break
+     case 4:this.checkfile = e.target.files[0]
+     break 
+   }
+ }
 
   verifiPassword() {
 
@@ -272,15 +288,7 @@ export class RegisterPage implements OnInit {
       event.preventDefault();
     }
   }
-  lettersOnlyValidation(event: any) {
-    const pattern = /[A-Za-z ÑñáéíóúÁÉÍÓÚ]/;
-    let inputChar = String.fromCharCode(event.charCode);
 
-    if (!pattern.test(inputChar)) {
-      // invalid character, prevent input
-      event.preventDefault();
-    }
-  }
   addressOnlyValidation(event: any) {
     const pattern = /[0-9A-Za-z ÑñáéíóúÁÉÍÓÚ]/;
     let inputChar = String.fromCharCode(event.charCode);
@@ -290,4 +298,13 @@ export class RegisterPage implements OnInit {
       event.preventDefault();
     }
   }
+  
+  validation(event:any, pattern){
+  let inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
 }
